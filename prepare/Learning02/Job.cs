4c5ec30d01538260eb491eede376b3d9a8438ae1
zacvong11 <von24001@byui.edu>
2024-09-26 00:49:15 -0600

    initial commit and push

---
+++ prepare/Learning02/Job.cs
@@ -0,0 +1,15 @@
+using System;
+
+public class Job
+    {
+        public string _company;
+        public string _jobTitle;
+        public int _startYear;
+        public int _endYear;
+        
+         public void display()
+        {
+            Console.WriteLine($"{_jobTitle} ({_company}) {_startYear} - {_endYear}");
+
+        }
+    }
--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,44 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        // Console.WriteLine("Hello world!");
+
+        Job job1 = new Job();
+        job1._company = "Lucid Software";
+        job1._jobTitle = "Account Executive";
+        job1._startYear = 2024;
+        job1._endYear = 2027;
+
+        // job1.display();
+
+        Job job2 = new Job();
+        job2._company = "Vanta";
+        job2._jobTitle = "Software Developer";
+        job2._startYear = 2023;
+        job2._endYear = 2025;
+
+        // job2.display();
+
+        Resume myResume = new Resume();
+        myResume._name = "Zac Vongphakdy";
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.display();
     }
 }
+
+//     public class Job
+//     {
+//         public string _company;
+//         public string _jobTitle;
+//         public int _startYear;
+//         public int _endYear;
+        
+//          public void display()
+//         {
+//             Console.WriteLine($"{_jobTitle} ({_company}) {_startYear} - {_endYear}");
+
+//         }
+//     }
+// }
